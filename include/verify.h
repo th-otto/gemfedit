7d404405a1a15dc7f59614fec29f6c472b1183d0
Thorsten Otto <admin@tho-otto.de>
2017-12-30 10:38:31 +0100

    Simplify verify.h

--- include/verify.h
+++ include/verify.h
@@ -1,15 +1,12 @@
-#ifndef _GL_VERIFY_H
-#define _GL_VERIFY_H
+#ifndef __VERIFY_H__
+#define __VERIFY_H__
 
+/* Concatenate two preprocessor tokens. */
+#ifndef __CONCAT
+#define __CONCAT(x, y) __CONCAT0(x, y)
+#define __CONCAT0(x, y) x##y
+#endif
 
-/* Concatenate two preprocessor tokens.  */
-#define _GL_CONCAT(x, y) _GL_CONCAT0 (x, y)
-#define _GL_CONCAT0(x, y) x##y
-
-/* _GL_COUNTER is an integer, preferably one that changes each time we
-   use it.  Use __COUNTER__ if it works, falling back on __LINE__
-   otherwise.  __LINE__ isn't perfect, but it's better than a
-   constant.  */
 #if defined __COUNTER__ && __COUNTER__ != __COUNTER__
 # define _GL_COUNTER __COUNTER__
 #else
@@ -18,22 +15,12 @@
 
 /* Generate a symbol with the given prefix, making it unique if
    possible.  */
-#define _GL_GENSYM(prefix) _GL_CONCAT (prefix, _GL_COUNTER)
-
-/* Verify requirement R at compile-time, as a declaration without a
-   trailing ';'.  If R is false, fail at compile-time, preferably
-   with a diagnostic that includes the string-literal DIAGNOSTIC.
-
-   Unfortunately, unlike C11, this implementation must appear as an
-   ordinary declaration, and cannot appear inside struct { ... }.  */
-
-# define _GL_VERIFY(R)                    \
-    extern int (*_GL_GENSYM (_gl_verify_function) (void))         \
-      [((R) ? 1 : -1)]
+#define __GENSYM(prefix) __CONCAT(prefix, _GL_COUNTER)
 
 /* Verify requirement R at compile-time, as a declaration without a
    trailing ';'.  */
 
-#define verify(R) _GL_VERIFY (R)
+#define verify(R)                    \
+    extern int (*__GENSYM (__verify_function) (void)) [((R) ? 1 : -1)]
 
 #endif
